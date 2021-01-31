89ebba35d3fce9206762f0e931cf4dfafb547742
armanullah <47667720+armanullah@users.noreply.github.com>
2021-01-31 14:44:22 -0500

    adding POJOS to project

---
+++ src/main/java/arman/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package arman.springframework.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/arman/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package arman.springframework.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
